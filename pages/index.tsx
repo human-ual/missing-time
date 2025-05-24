b0c7a8047ddaa3ee857b08fb2fc054ba6a0c2890
human-ual <humanual21@gmail.com>
2025-05-24 15:47:05 +0800

    index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,26 @@
 // pages/index.tsx
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center space-y-6 p-6 text-center">
       <h1 className="text-3xl font-bold">Missing Time</h1>
       <p className="text-lg">想念的時候就抽籤</p>
       <div className="flex gap-4">
-        <a
+        <Link
           href="/draw"
           className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700"
         >
           今天做什麼好呢~
-        </a>
-        <a
+        </Link>
+        <Link
           href="/submit"
           className="px-4 py-2 bg-green-600 text-white rounded hover:bg-green-700"
         >
           投稿任務
-        </a>
+        </Link>
       </div>
     </div>
   );
 }
+
