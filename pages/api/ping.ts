58bb9daeece109d81b1f18fb521102f346346c1c
human-ual <youremail@example.com>
2025-05-30 01:52:09 +0800

    feat: add ping endpoint to prevent project from being paused

---
+++ pages/api/ping.ts
@@ -0,0 +1,6 @@
+// pages/api/ping.ts
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  res.status(200).json({ message: "pong" });
+}
